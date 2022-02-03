64dba16131b2758a5e2748c2c88e174cb2eb33a2
Seungwoo Yoo <ryuryu10@naver.com>
2022-02-03 23:15:02 +0900

    분초 불러오기 기능 #1

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ from datetime import date, time, timedelta
 from selenium import webdriver
 
 import json
+import time
 
 _URL = "http://wakatime.com/dashboard"
 
-week = []
-week_time = []
+Last_7_Days_Date_List = []
+Last_7_Days_Time_List = []
+Last_7_Days_Second_List = []
+
 today = date.today()
 print(type(date.today() - timedelta(1)))
 for numbers in range(6,0,-1):
-    week.append(date.today() - timedelta(numbers))
-week.append(date.today())
+    Last_7_Days_Date_List.append(date.today() - timedelta(numbers))
+Last_7_Days_Date_List.append(date.today())
 
 
 options = webdriver.ChromeOptions()
@@ -30,25 +33,24 @@ driver.implicitly_wait(10)
 print("Login wait")
 p_tag = WebDriverWait(driver,timeout=999).until(EC.presence_of_element_located((By.CLASS_NAME, "bb-chart")))
 print("Login End")
-for day in week:
+for day in Last_7_Days_Date_List:
     driver.get(f'http://wakatime.com/dashboard/day?date={day}')
     driver.implicitly_wait(10)
     times = driver.find_element_by_xpath('//*[@id="total-logged-time"]/b').text
-    week_time.append(times)
+    Last_7_Days_Time_List.append(times)
 driver.quit()
-for times in week_time:
+
+for times in Last_7_Days_Time_List:
     splited = times.split(" ")
     if len(splited) == "2":
         #mins data
         CODE_hour = 0
         CODE_min = splited[0]
+        converted_time = time(0, CODE_min, 0)
     elif len(splited) == "4":
         #hours, mins data
         CODE_hour = splited[0]
         CODE_min = splited[2]
+        converted_time = time(CODE_hour, CODE_min)
     else:
-        raiseExceptions("None data.")
-    print(splited)
-f = open('./dump.json')
-f.write(CODE_hour, CODE_min)
-f.close()
+        print("ERROR", splited,len(splited))
