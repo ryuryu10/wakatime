2af3da1dda970523ee24e0134bf9e3de4870b82a
Seungwoo Yoo <ryuryu10@naver.com>
2022-02-05 20:43:04 +0900

    표 생성 기능 완성

--- Export/app.py
+++ Export/app.py
@@ -28,6 +28,28 @@ def main():
         sidebar2 = f'{sbar1[0]}h {sbar1[1]}m',
         sidebar3 = f'{sbar2[0]}h {sbar2[1]}m',
         sidebar4 = f'{sbar3[0]}h {sbar3[1]}m',
+        date1 = OPENED_json['DATAS']['down_bar'][0],
+        date2 = OPENED_json['DATAS']['down_bar'][1],
+        date3 = OPENED_json['DATAS']['down_bar'][2],
+        date4 = OPENED_json['DATAS']['down_bar'][3],
+        date5 = OPENED_json['DATAS']['down_bar'][4],
+        date6 = OPENED_json['DATAS']['down_bar'][5],
+        date7 = OPENED_json['DATAS']['down_bar'][6],
+        height1 = OPENED_json['DATAS']['graphs'][0],
+        height2 = OPENED_json['DATAS']['graphs'][1],
+        height3 = OPENED_json['DATAS']['graphs'][2],
+        height4 = OPENED_json['DATAS']['graphs'][3],
+        height5 = OPENED_json['DATAS']['graphs'][4],
+        height6 = OPENED_json['DATAS']['graphs'][5],
+        height7 = OPENED_json['DATAS']['graphs'][6],
+        position1 = 567 - int(OPENED_json['DATAS']['graphs'][0]),
+        position2 = 567 - int(OPENED_json['DATAS']['graphs'][1]),
+        position3 = 567 - int(OPENED_json['DATAS']['graphs'][2]),
+        position4 = 567 - int(OPENED_json['DATAS']['graphs'][3]),
+        position5 = 567 - int(OPENED_json['DATAS']['graphs'][4]),
+        position6 = 567 - int(OPENED_json['DATAS']['graphs'][5]),
+        position7 = 567 - int(OPENED_json['DATAS']['graphs'][6])
+        
         
         )
 
---
+++ function/image3.py
@@ -0,0 +1,2 @@
+def maker():
+    paas
