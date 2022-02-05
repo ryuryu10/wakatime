d0897413f7295c4a9b9833f3c24ecf9894c32887
Seungwoo Yoo <ryuryu10@naver.com>
2022-02-05 13:15:26 +0900

    차트 사이트바 생성 기능 추가

--- function/image1.py
+++ function/image1.py
@@ -1,3 +1,7 @@
 def maker(seconds_data):
-    seconds_data/4
-    pass
+    sidebar = [0,max(seconds_data)/3,(max(seconds_data)/3)*2,(max(seconds_data)/3)*3]
+    print(sidebar)
+    for a in seconds_data:
+        temp = 600 * a
+        print(temp/ max(seconds_data))
+    pass                                                                             
